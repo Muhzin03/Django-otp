b3c1af5694400f412993c9c749bd2af708cb4176
Muhzin03 <muhzinkm10@gmail.com>
2023-04-14 21:22:04 +0530

    fixed issue

--- authentication/admin.py
+++ authentication/admin.py
@@ -5,6 +5,8 @@ from .models import CustomUser
 
 class UserAdmin(BaseUserAdmin):
     list_display = ('email', 'first_name', 'last_name', 'is_active', 'is_staff', 'is_superuser')
+    ordering = ('email',)
+
    
 # Register your models here.
 admin.site.register(CustomUser, UserAdmin)
