be9284cc465ca2e5021cbd343717c7bb6b926837
Muhzin03 <muhzinkm10@gmail.com>
2023-04-14 15:30:03 +0530

    work done for wooden clouds

---
+++ authentication/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import CustomUser
+
+
+class UserAdmin(BaseUserAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'is_active', 'is_staff', 'is_superuser')
+   
+# Register your models here.
+admin.site.register(CustomUser, UserAdmin)
